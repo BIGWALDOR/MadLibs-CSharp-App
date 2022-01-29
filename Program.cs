c385ff550dc594c4b0875d48b6c9c81c35d094a8
Walid Bouladam <bouladam.walid86@gmail.com>
2022-01-29 22:19:59 +1300

    Declared string vars; allowed user to define vars

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string colour, pluralNoun, celebrity;
+
+Console.Write("Enter a colour: ");
+colour = Console.ReadLine();
+
+Console.Write("Enter a pluralNoun: ");
+pluralNoun = Console.ReadLine();
+
+Console.Write("Enter a celebrity: ");
+celebrity = Console.ReadLine();
+
+
+
+Console.WriteLine("Roses are " + colour);
+Console.WriteLine(pluralNoun + " are blue");
+Console.WriteLine("I love " + celebrity);
+
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MadLibs-CSharp-App
+
 A basic mad libs game app built using the C# language
+
+Another simple app built using the C# language to help reinforce the basic functionality of C#, through a mad libs game app.
